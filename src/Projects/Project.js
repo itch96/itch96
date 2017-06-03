5eee46ed0a0474737041b537a7c0ed2ada7e43cf
Rahul Gupta <useritch96@gmail.com>
2017-06-03 13:12:56 +0530

    Fixed

--- src/Projects/Project.js
+++ src/Projects/Project.js
@@ -6,7 +6,7 @@ class Project extends React.Component {
     render() {
         return (
             <Tilt className="Tilt project col-xs-10 col-xs-offset-1 col-md-3">
-                <a href={this.props.target} target='blank'>
+                <a href={this.props.target}>
                     <img className='Tilt-inner' src={this.props.image} alt={this.props.name}/>
                     <h3>{this.props.name}</h3>
                     <p>{this.props.description}</p>
